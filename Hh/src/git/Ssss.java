e0cee3721e471a2190fdc952bc074c27bf5f014d
Partho <Partho@192.168.1.12>
2018-06-18 12:32:08 +0530

    lalu

---
+++ Hh/src/git/Ssss.java
@@ -0,0 +1,12 @@
+package git;
+
+public class Ssss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello");
+
+System.out.println("huhuh");
+	}
+
+}
